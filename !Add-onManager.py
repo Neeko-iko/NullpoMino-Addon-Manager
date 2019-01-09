4d93f8a6b9aa66814aca8336847825f6bb4d26d5
Oshisaure <46495678+Oshisaure@users.noreply.github.com>
2019-01-10 00:11:12 +0100

    Changed config handing system

--- !Add-onManager.py
+++ !Add-onManager.py
@@ -1,57 +1,98 @@
 import os
 import fnmatch
+import re
+
+SETTINGS = {
+    # Dictionary of all settings with their default options, by category
+    "Directories" : [
+        ["NullpoMino Installation Folder", "C:\\NullpoMino"],
+        ["Custom Add-ons Folder", "none"],
+        ["Custom Visuals Folder", "none"],
+        ["Custom Sounds Folder", "none"]
+    ],
+    "Config (F = False | T = True)" : [
+        ["Reset on next launch", "F"],
+        ["Check for Blockskins", "T"]
+    ]
+}
+
 def fullsetup():
     print("Starting set-up routine...")
     configfile = open("config.txt", "w+")
-    configfile.write("Directories:\n")
+    configfile.write("#Directories:\n")
         #   asks where stuff is
-    for directorytype in ["please copy-paste your NullpoMino directory (NOT YOUR RES FOLDER) \n[Enter nothing for the default]\n Adder NullpoMino res Folder: Default C:\\NullpoMino\\res", "please copy-paste your NullpoMino add-on folder (IF YOU HAVE ONE) \n[Enter nothing for the default]\n Adder Custom Add-ons Folder: Default None"]:
-            directory = input(directorytype[:99])
-                #   Checks for if the user says literally nothing
-            if directory == "" or directory.upper() == "NOTHING":
-                directory = directorytype[106:128] + directorytype[136:]
-            else:
-                directory = directorytype[106:129] + directory
-            configfile.write(directory + "\n")
-    if directory[-4:].upper() == "NONE":
-        configfile.write("Visual Folder: Skins\nSounds Folder: Sound Effects")
-    else:
-        #   Asks what your visual folder and sound folders are called, if you have them.
-        for directorytype in ["What's the name of your graphics folder?\nAdder Visual Folder: ", "What's the name of your sounds folder?\n  Adder Sounds Folder: "]:
-            directory = input(directorytype[:41])
-            directory = directorytype[47:62] + directory
-            configfile.write(directory + "\n")
-    configfile.write("\nConfig:  (F = False | T = True)\nReset on next launch = F\nCheck for Blockskins = T")
+    for directorytype in SETTINGS["Directories"]:
+        directory = input(
+            "Please enter the full path to your {0}.\n[Enter nothing for the default ({1})] "
+            .format(directorytype[0], "none" if directorytype[1] == "" else directorytype[1])
+        )
+            #   Checks for if the user says literally nothing
+        if directory == "" or directory.upper() == "NOTHING":
+            directory = directorytype[1]
+        configfile.write("{0} = {1}\n".format(directorytype[0], directory))
+    configfile.write("\n#Config (F = False | T = True)\n")
+    for config in SETTINGS["Config (F = False | T = True)"]:
+        configfile.write("{0} = {1}\n".format(*config))
+    configfile.close()
+    
+def readconfig():
+    configfile = open("config.txt", "r")
+    match = re.findall("(?:([^#\n][^\n]*?)\s+=\s+([^\n]+))", configfile.read())
+    dict = {}
+    for key, val in match:
+        dict[key] = val
+    configfile.close()
+    return dict
+
+def writeconfig(config):
+    configfile = open("config.txt", "w+")
+    configfile.write("#Directories:\n")
+    for directorytype in SETTINGS["Directories"]:
+        configfile.write("{0} = {1}\n".format(directorytype[0], config[directorytype[0]]))
+    configfile.write("#Config (F = False | T = True)\n")
+    for settingtype in SETTINGS["Config (F = False | T = True)"]:
+        configfile.write("{0} = {1}\n".format(settingtype[0], config[settingtype[0]]))
+    configfile.close()
+    
+# ###############################################################
 
 print("Checking for config...")
 try:
-    configfile = open("config.txt", "r")
+    configdata = readconfig()
     print("Config found! applying settings right now...")
 
 except FileNotFoundError:
     print("No config found! That's no big deal. We'll get one set up, it'll only take a few minutes at most!")
     fullsetup()
-if configdata[7][25] = "T":
-    fullsetup()
-configdata = configfile.readlines
-resFolder = configdata[1][24:]
-custom = configdata[2][24:]
-visfolder = configdata[3][15:]
-sefolder = configdata[4][15:]
-configfile.close
+    configdata = readconfig()
 
-if custom.upper != "NONE":
+print(configdata)
+try:
+    if configdata["Reset on next launch"] == "T":
+        fullsetup()
+    resFolder = configdata["NullpoMino Installation Folder"]
+    custom = configdata["Custom Add-ons Folder"]
+    visfolder = configdata["Custom Visuals Folder"]
+    sefolder = configdata["Custom Sounds Folder"]
+except KeyError:
+    print("The config file seems corrupted. Let's fix this by going through the set up again.")
+    fullsetup()
+    configdata = readconfig()
+    resFolder = configdata["NullpoMino Installation Folder"]
+    custom = configdata["Custom Add-ons Folder"]
+    visfolder = configdata["Custom Visuals Folder"]
+    sefolder = configdata["Custom Sounds Folder"]
+
+if custom.upper() != "NONE":
     custom = custom + "\\"
 print("Checking Folders...")
-if configdata[8][40] = "T":
+if configdata["Check for Blockskins"] == "T":
     if os.path.isdir(custom + "blockskin") == False:
         permission = input("There isn't a 'blockskin' folder in your custom directory \n\nwould you mind if I created one? - Y/N \n If you say 'no', I won't ask you again, and I won't check for blockskins on launch. \n\n if you say 'yes' i will create one and check it for blockskins")
         if permission[0].upper == "N":
             print("Alrighty, I won't bother you about blockskins anymore - Lemme just update your prefernces really fast...")
-            configdata[8] = "Check for Blockskins = F\n"
-            configfile = open("config.txt" "w+")
-            configfile.write(configdata)
-            configfile.close
+            configdata["Check for Blockskins"] = "F"
+            writeconfig()
         else:
             print("Thanks! I'll make sure to add one right away.")
     
