4bd4d134d27940e521cf14c98934de11fcefd893
Oshisaure <46495678+Oshisaure@users.noreply.github.com>
2019-01-17 00:56:14 +0100

    Now successfully moves blockskins!

--- !Add-onManager.py
+++ !Add-onManager.py
@@ -70,20 +70,11 @@ def writeconfig(config):
 # ###############################################################
 
 # #################  Function to add blockskins  ################
-def BlockskinAdd():
-    
-                #   grabs blockskin count (from the normal blockskins.)
-        BSkinpath = resFolder + "\\graphics\\blockskin\\normal"
-        NumBSkins = len((fnmatch.filter(os.listdir(BSkinpath), '*.png')))
-        print('Found ' + str(len((fnmatch.filter(os.listdir(custom + "blockskin\\normal"), '*.png')))) + ' blockskins!')
+def BlockskinMove(origin, id, dest):
 
-                #   edits the blockskins in their folders to be sequential to what NullpoMino has
-        for size in ["\\small", "\\normal", "\\big"]:
-
-            num = NumBSkins
-            skinpath = custom + "blockskin\\" + size
-            #for fileName in os.listdir(skinpath):
-                #os.renames(fileName, fileName.replace(skinpath[11], skinpath[11] + str(num)))
+        #   edits the blockskins in their folders to be sequential to what NullpoMino has
+        for size in sizes:
+            os.renames(origin+size+".png", dest+size+size[0:2]+str(id)+".png")
 
 # ##################################################################
 # Actual code:
@@ -134,13 +125,15 @@ if configdata["Check for Blockskins"] == "T":
         else:
             print("Thanks! I'll make sure to add one right now.")
             # Create the folder, instead of lie ##
-
+            os.mkdir(custom + "blockskin")
+    """
         #   Checks the different sized folders to make sure they exist, if they do, add them
-    for size in sizes: 
-        ''
-        ######## unfinished.
+    for size in sizes:
+        if not os.path.isdir(f"{custom}blockskin{size}"):
+            os.mkdir(f"{custom}blockskin{size}")
 
         #   Checks for any new files in the custom blockskin folders
+        
     consistent = []
     for size in sizes:
         consistent.append(True if len((fnmatch.filter(os.listdir(f"{custom}blockskin{size}"), '*.png'))) != 0 else False)
@@ -151,12 +144,36 @@ if configdata["Check for Blockskins"] == "T":
     elif consistent == [True, True, True]:
         print("new blockskins found!")
         BlockskinAdd()
-    else:
-        permission = input("You seem to have a mix-up of blockskins..\n"
-        "I can still try and add them for you, but NullpoMino might not be too happy with it\n\n Y/N  ")
-        if permission[0].upper() == "N":
-            print("Aighty! I'll leave them be.")
-        else:
-            print("okay, I'll try, don't blame me if NullpoMino stops working for whatever reason..")
-            BlockskinAdd()
+        """
+    NullSkinPath = resFolder + "\\graphics\\blockskin"
+    NullSkinNum = len(fnmatch.filter(os.listdir(NullSkinPath + "\\normal"), '*.png'))
+    print(f"Detected {NullSkinNum} blockskins in NullpoMino folder.")
+    for dir in os.listdir(custom + "blockskin"):
+        workingDir = custom + "blockskin\\" + dir
+        # print(workingDir)
+        if os.path.isdir(workingDir):
+            consistent = [os.path.exists(f"{workingDir}{s}.png") for s in sizes]
+            if consistent == [True, True, True]:
+                print(f"Adding blockskin {dir}.")
+                BlockskinMove(workingDir, NullSkinNum, NullSkinPath)
+                NullSkinNum += 1
+                for file in os.listdir(workingDir):
+                    os.remove(f"{workingDir}\\{file}")
+                os.rmdir(workingDir)
+            elif consistent == [False, False, False]:
+                print(f"Skipping empty blockskin {dir}.")
+                for file in os.listdir(workingDir):
+                    os.remove(f"{workingDir}\\{file}")
+                os.rmdir(workingDir)
+            else:
+                print("There seems to be files missing in blockskin {dir}.")
+                permission = ""
+                while permission not in ["Y", "N"]:
+                    permission = input("Do you want to add the blockskin to NullpoMino anyway? (Y/N)").upper()
+                if permission == "N":
+                    print("Aighty! I'll leave them be.")
+                else:
+                    print("okay, I'll try, don't blame me if NullpoMino stops working for whatever reason..")
+                    BlockskinMove(workingDir, NullSkinNum, NullSkinPath)
+                    NullSkinNum += 1
 # 
